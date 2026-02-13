cd9de200f2b6e4c06c5b71cad2d483046097f799
softsurgery <109615722+softsurgery@users.noreply.github.com>
2026-02-13 21:59:23 +0100

    refactor(sidebar): standardize formatting and improve readability

--- libs/ui/sidebar/src/lib/hlm-sidebar-wrapper.ts
+++ libs/ui/sidebar/src/lib/hlm-sidebar-wrapper.ts
@@ -1,22 +1,27 @@
-import { Directive, input } from '@angular/core';
-import { classes } from '@spartan-ng/helm/utils';
-import { injectHlmSidebarConfig } from './hlm-sidebar.token';
+import { Directive, input } from "@angular/core";
+import { classes } from "@spartan-ng/helm/utils";
+import { injectHlmSidebarConfig } from "./hlm-sidebar.token";
 
 @Directive({
-	selector: '[hlmSidebarWrapper],hlm-sidebar-wrapper',
-	host: {
-		'data-slot': 'sidebar-wrapper',
-		'[style.--sidebar-width]': 'sidebarWidth()',
-		'[style.--sidebar-width-icon]': 'sidebarWidthIcon()',
-	},
+  selector: "[hlmSidebarWrapper],hlm-sidebar-wrapper",
+  host: {
+    "data-slot": "sidebar-wrapper",
+    "[style.--sidebar-width]": "sidebarWidth()",
+    "[style.--sidebar-width-icon]": "sidebarWidthIcon()",
+  },
 })
 export class HlmSidebarWrapper {
-	private readonly _config = injectHlmSidebarConfig();
+  private readonly _config = injectHlmSidebarConfig();
 
-	public readonly sidebarWidth = input<string>(this._config.sidebarWidth);
-	public readonly sidebarWidthIcon = input<string>(this._config.sidebarWidthIcon);
+  public readonly sidebarWidth = input<string>(this._config.sidebarWidth);
+  public readonly sidebarWidthIcon = input<string>(
+    this._config.sidebarWidthIcon,
+  );
 
-	constructor() {
-		classes(() => 'group/sidebar-wrapper has-[[data-variant=inset]]:bg-sidebar flex min-h-dvh w-full');
-	}
+  constructor() {
+    classes(
+      () =>
+        "group/sidebar-wrapper has-[[data-variant=inset]]:bg-sidebar flex min-h-dvh w-full",
+    );
+  }
 }
